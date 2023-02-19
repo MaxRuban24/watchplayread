01f4d63c745bcc215b0720ebc7a1561097d5afb1
max <max@metalgram.onmicrosoft.com>
2023-02-19 13:34:00 +0200

    updating env file and docker compose

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
